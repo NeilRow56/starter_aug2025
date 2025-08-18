686f8b13c9ddb098c9997c2bb37f7eb00c74c8c6
NeilRow56 <neilrowland56@gmail.com>
2025-08-18 08:57:56 +0100

    Better auth installed with Get Dtarted and Sign Out buttons. Email verification and Reset password

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,8 @@
-import { config } from 'dotenv'
 import { drizzle } from 'drizzle-orm/neon-http'
-import { schema } from './schema'
+import { neon } from '@neondatabase/serverless'
 
-config({ path: '.env' }) // or .env.local
+import * as schema from './schema'
 
-export const db = drizzle(process.env.DATABASE_URL!, { schema })
+const sql = neon(process.env.DATABASE_URL!)
+
+export const db = drizzle(sql, { schema })
